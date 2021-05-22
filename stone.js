661f898c66033f01603755c25e70f11dc0686c68
aa195 <78353803+aa195@users.noreply.github.com>
2021-05-22 14:39:34 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,12 @@
+class Stone extends mango{ 
+
+    constructor(x,y){
+        super(x,y,250,250);
+        this.image = loadImage("images/stone.png");
+    }
+
+    display(){
+        super.display();
+    }
+
+}
